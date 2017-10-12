5b33eea00932a998c20b4b9a12d8d7df0be0326a
Kieran Bamforth <hello@kieranbamforth.me>
2017-10-12 23:21:29 +0100

    here be an update.\

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 const httpProxy = require('http-proxy');
 
 const proxy = httpProxy.createProxyServer({
-  target: process.env.TARGET,
-  port: process.env.PORT
-}).listen(8000); // See (†)
-
-
-proxy.on('proxyReq', (proxyReq, req, res, options) => {
-  debugger;
-});
+  target: {
+    url: process.env.TARGET,
+    port: process.env.PORT,
+    cert: process.env.CERT,
+    key: process.env.KEY
+  },
+}).listen(8000);
