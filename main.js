921742410f6f52c696e36d1604192ed28522e5dd
Kieran Bamforth <hello@kieranbamforth.me>
2017-10-24 16:07:11 +0100

    Using SSL certs

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const httpProxy = require('http-proxy');
+const https = require('https');
+const fs = require('fs');
+
+console.log( process.env.CERT);
+console.log( process.env.KEY);
 
 const proxy = httpProxy.createProxyServer({
-  target: {
-    url: process.env.TARGET,
-    port: process.env.PORT,
-    cert: process.env.CERT,
-    key: process.env.KEY
-  },
+  target: process.env.TARGET,
+  agent: new https.Agent({
+    cert: fs.readFileSync(process.env.CERT),
+    key: fs.readFileSync(process.env.KEY),
+  }),
+  secure: false
 }).listen(8000);
