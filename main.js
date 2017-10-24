f69354099f07fcf148c24e338029514f75f9a1fb
Kieran Bamforth <hello@kieranbamforth.me>
2017-10-24 16:09:37 +0100

    removing consolelogs

--- main.js
+++ main.js
@@ -2,9 +2,6 @@ const httpProxy = require('http-proxy');
 const https = require('https');
 const fs = require('fs');
 
-console.log( process.env.CERT);
-console.log( process.env.KEY);
-
 const proxy = httpProxy.createProxyServer({
   target: process.env.TARGET,
   agent: new https.Agent({
